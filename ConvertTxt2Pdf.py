a61015074a538acbb6f47e8c38ecfb29e5507ecd
Yoshiwo <is0383kk@gmail.com>
2024-07-08 17:28:41 +0900

    Update ConvertTxt2Pdf.py

--- ConvertTxt2Pdf.py
+++ ConvertTxt2Pdf.py
@@ -2,26 +2,36 @@ import os
 from fpdf import FPDF
 
 class PDF(FPDF):
-    def __init__(self):
-        super().__init__()
-        self.add_page()
-        self.set_auto_page_break(auto=True, margin=15)
-        self.set_font("Arial", size=12)
+    # Header and Footer
+    # If you can use Header and Footer, you can use the following code
+    
+    '''
+    def header(self):
+        self.set_font('helvetica', '', 10)
+        self.cell(0, 10, 'txt to PDF Converter', 0, 1, 'C')
 
-    def add_txt_content(self, txt_content):
-        for line in txt_content.split('\n'):
-            self.cell(200, 10, txt=line.encode('latin-1', 'replace').decode('latin-1'), ln=True)
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('helvetica', '', 8)
+        self.cell(0, 10, f'Page {self.page_no()}', 0, 0, 'C')
+    '''
 
 def txt_to_pdf(txt_path, pdf_path):
     # Open txt file with UTF-8 encoding
     with open(txt_path, 'r', encoding='utf-8') as txt_file:
         txt_content = txt_file.read()
 
-    # Initialize the PDF
     pdf = PDF()
-    pdf.add_txt_content(txt_content)
+    pdf.set_font('helvetica', size=10)
+    pdf.add_page()
+    pdf.set_auto_page_break(auto=True, margin=15)
 
-    # Convert        
+    for line in txt_content.split('\n'):
+        line = line.replace('\t', '    ')
+        pdf.cell(w=0, h=pdf.font_size * 1.5 ,text=line.encode('utf-8').decode('utf-8'))
+        pdf.ln()
+    
+    # Convert
     try:
         pdf.output(pdf_path)
         print(f"Success：{txt_path} => {pdf_path}")
